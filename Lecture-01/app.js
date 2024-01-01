0c442db2b7c78de4ef512637a079c2ea07ed359e
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2024-01-01 20:57:34 +0500

    lecture 01 complete

--- Lecture-01/app.js
+++ Lecture-01/app.js
@@ -1,9 +1,106 @@
 // variables : A variable is simply the name of a storage location.
 
-var age = 26;
-var city = "Karachi"
+// var age = 26;
+// var city = "Karachi"
 
 //Data Types in JS  Primitive types
 
 // 1. Number  2.Boolean  3. String  4. Undefine  5. Null
+
+// Number in JS
+
+// Positive, Negative, Floating Number with decimal
+
+// Operators in Js
+
+// Addition (a+b)
+// subtraction (a-b)
+// multiplication (a*b)
+// division   (a/b)
+// modulo   (%) ==> remainder operator
+// exponentiation  (**) ==> power operator
+
+// Nan in JS
+
+// Nan Not a Number
+// 0/0
+// Nan -1
+// Nan +1
+// Nan + Nan
+
+// Operator Precedence  (BODMAS rule)
+
+// ()
+// **
+// *, /, %
+// +, -
+
+// Assignment operator
+
+age = age +1;
+age += 1;
+
+age = age -1;
+age -= 1;
+
+age = age * 3;
+age *= 3;
+
+age = age / 2;
+age /= 2;
+
+age = age % 2;
+age %= 2;
+
+
+// unary operator 
+
+age ++;
+age --;
+
+
+// way to write js  (mostly use camelCase)
+
+// 1. camelCase
+// 2. snake_case
+// 3. PascalCase
+
+// Boolean in JS  (true or false)
+
+// let age = 23;
+// let isAdult = true;
+
+// let age = 13;
+// let isAdult = false;
+
+
+// TypeOf
+
+// var a = 5;
+// typeof(a);
+
+// var b = true;
+// typeof(b);
+
+// string in JS
+
+// var str = "Nasir"
+// let strn = "khan";
+// const a = 'ahmed';
+
+// concatenation  (adding string togather)
+
+// let a = 'nasir';
+// let b = 'khan';
+
+// console.log(a + b);
+
+
+
+
+
+
+
+
+
 
