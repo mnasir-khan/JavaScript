66331201e5d2a20776f290b0e05b5b82dbebaf66
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2024-01-05 19:18:23 +0500

    Lecture complete

--- Lecture-07/app.js
+++ Lecture-07/app.js
@@ -1,21 +1,21 @@
 // this keyword
 
-const student = {
-    name : "Nasir",
-    age : 26,
-    eng : 75,
-    pys : 85,
-    math : 100,
-    getAvg(){
-        console.log(this)
-        let avg = (this.eng + this.pys + this.math)/3;
-        console.log(`${this.name} got avg marks = ${avg}`);
-    }
-}
+// const student = {
+//     name : "Nasir",
+//     age : 26,
+//     eng : 75,
+//     pys : 85,
+//     math : 100,
+//     getAvg(){
+//         console.log(this)
+//         let avg = (this.eng + this.pys + this.math)/3;
+//         console.log(`${this.name} got avg marks = ${avg}`);
+//     }
+// }
 
-function getAvg(){
-    console.log(this)
-}
+// function getAvg(){
+//     console.log(this)
+// }
 
 //  try and catch
 
@@ -114,11 +114,11 @@ console.log("Hello 5")
 // }, 4000);
 // console.log("Welcome to");
 
-console.log("salam");
-setTimeout(()=>{
-    console.log("Nasir")
-}, 4000)
-console.log("Khush Amdid");
+// console.log("salam");
+// setTimeout(()=>{
+//     console.log("Nasir")
+// }, 4000)
+// console.log("Khush Amdid");
 
 
 // SetIntervall
@@ -127,15 +127,79 @@ console.log("Khush Amdid");
 //     console.log("Nasir");
 // }, 2000);
 
-let id = setInterval(()=>{
-        console.log("Nasir");
-    }, 2000);
+// let id = setInterval(()=>{
+//         console.log("Nasir");
+//     }, 2000);
     
-    clearInterval(id);
+//     clearInterval(id);
 
-let id1 = setInterval(()=>{
-        console.log("Khan");
-    }, 1500);
+// let id1 = setInterval(()=>{
+//         console.log("Khan");
+//     }, 1500);
     
-    clearInterval(id1);
+//     clearInterval(id1);
+
+
+// this with arrow functions
+
+const student = {
+    name : "Aman",
+    marks : 95,
+    prop : this, // global scope
+    getName : function(){
+        console.log(this); 
+        return this.name;
+    },
+    getMarks: ()=>{
+        console.log(this); // Parent scope --> window
+        return student.marks
+    },
+    getInfo1 : function(){
+        setTimeout(()=>{
+            console.log(this) // student 
+        }, 2000);
+    },
+    getInfo2 : function(){
+        setTimeout (function(){
+            console.log(this); // window
+        }, 2000);
+    }
+}
     
+
+// practice Qs
+// Qs1
+
+// method 01
+const square = (n)=>{
+    return n*n;
+}
+console.log(square(4));
+console.log(square(5));
+console.log(square(6));
+
+// method 2
+const squae = (n)=>(n*n);
+
+console.log(squae(9));
+console.log(squae(8));
+console.log(squae(7));
+
+//method 3
+const squa = n=>n*n;
+
+console.log(squa(10));
+console.log(squa(11));
+console.log(squa(12));
+
+
+// Qs 2
+
+let id = setInterval(() => {
+    console.log("hello world")
+},2000);
+
+setTimeout(()=>{
+    clearInterval(id);
+    console.log('clear interval run')
+},10000);
