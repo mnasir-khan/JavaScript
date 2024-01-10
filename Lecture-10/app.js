0223d5995b6621deea1510455032edff07e074c5
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2024-01-10 12:55:28 +0500

    25% lecture complete

---
+++ Lecture-10/app.js
@@ -0,0 +1,138 @@
+// _________DOM Events___________
+
+// ***********onclick
+
+// let btn = document.querySelector("button");
+
+// console.log(btn);
+// console.dir(btn);
+
+// btn.onclick = function(){
+    //     console.log("button was clicked")
+    //     alert("button was clicked")
+    // }
+    
+    // function sayHello (){
+        //     // console.log('Hello');
+    //     alert('Hello');
+    // }
+    
+    // btn.onclick = sayHello;
+    
+    
+    // let btns = document.querySelectorAll("button");
+    // // console.log(btns);
+    // console.dir(btns);
+    
+    // for(btns of btns){
+    //     btns.onclick = sayallhello;
+    // }
+    
+    // function sayallhello (){
+    //     console.log('hello');
+    //     alert("hello");
+    // }
+    
+    // btns.onclick = sayallhello;
+
+
+    // ***********on mouse enter*****
+
+    // let btns = document.querySelectorAll("button");
+    // // console.log(btns);
+    // console.dir(btns);
+    
+    // for(btns of btns){
+    //     btns.onclick = sayallhello;
+    //     btns.onmouseenter = function(){
+    //         console.log('you enter a button')
+    //     }
+    // }
+
+    // function sayallhello (){
+    //     console.log('hello');
+    //     alert("hello");
+    // }
+    
+    // btns.onclick = sayallhello;
+
+
+    //________Event Listners________
+
+    // let btns = document.querySelectorAll("button");
+  
+    
+    // for(btns of btns){
+    //     btns.onclick = sayname; // onclick k method me hum sirf 1 kam krwa sakty hn..
+    //     btns.onclick = sayallhello; // ya "sayname" or ya "sayallhello"
+    //    }
+
+       
+    //    function sayname (){
+    //        alert("nasir");
+    //     }
+        
+    //     function sayallhello (){
+    //         alert("hello");
+    //     }
+
+ 
+// ******addEventListner
+
+// //single click
+// let btns = document.querySelectorAll("button");
+  
+    
+//     for(btn of btns){
+//         // btns.onclick = sayname;
+//         // btns.onclick = sayallhello;
+//         btn.addEventListener('click', sayallhello)
+//         btn.addEventListener('click', sayname)
+//        }
+
+//        function sayname (){
+//            alert("nasir");
+//         }
+        
+//         function sayallhello (){
+//             alert("hello");
+//         }
+
+// //double click
+// let btns = document.querySelectorAll("button");
+  
+    
+//     for(btn of btns){
+//         // btns.onclick = sayname;
+//         // btns.onclick = sayallhello;
+//         // btn.addEventListener('dblclick', sayallhello)
+//         btn.addEventListener('dblclick', function (){
+//             console.log("you clicked twice")
+//         })
+//        }
+
+//        function sayname (){
+//            alert("nasir");
+//         }
+        
+//         function sayallhello (){
+//             alert("hello");
+//         }
+
+//mouse enter
+let btns = document.querySelectorAll("button");
+  
+    
+    for(btn of btns){
+        btn.addEventListener('mouseenter', function (){
+            console.log("you clicked enter")
+        })
+       }
+
+       function sayname (){
+           alert("nasir");
+        }
+        
+        function sayallhello (){
+            alert("hello");
+        }
