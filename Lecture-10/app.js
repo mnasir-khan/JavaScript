9cee8c13bbfb4ca3c313d06d398b57122b873bbd
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2024-01-10 17:40:19 +0500

    60% lecture complete

--- Lecture-10/app.js
+++ Lecture-10/app.js
@@ -120,19 +120,123 @@
 //         }
 
 //mouse enter
-let btns = document.querySelectorAll("button");
+// let btns = document.querySelectorAll("button");
   
     
-    for(btn of btns){
-        btn.addEventListener('mouseenter', function (){
-            console.log("you clicked enter")
-        })
-       }
+//     for(btn of btns){
+//         btn.addEventListener('mouseenter', function (){
+//             console.log("you clicked enter")
+//         })
+//        }
 
-       function sayname (){
-           alert("nasir");
-        }
+//        function sayname (){
+//            alert("nasir");
+//         }
         
-        function sayallhello (){
-            alert("hello");
-        }
+//         function sayallhello (){
+//             alert("hello");
+//         }
+
+
+//________________________________________________________________________________________
+
+////________________________Activity____________________
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandom();
+//     h3.innerText = randomColor;
+
+//     let div = document.querySelector("div");
+//     div.style.backgroundColor = randomColor;
+
+//     console.log("color updated")
+// })
+
+// function getRandom(){
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
+
+//     let color = `rgb(${red}, ${green}, ${blue})`
+//     return color;
+// }
+
+
+// ______________________Event Listener for Element_____________
+
+// let p = document.querySelector("p");
+
+// p.addEventListener("click", function(){
+//     console.log("Paragraph was clicked")
+// });
+
+// let div = document.querySelector('.box');
+// div.addEventListener('mouseenter', function(){
+//     console.log("mouse inside box");
+// });
+
+
+// _____________this in Event Listening________________
+
+// let btn = document.querySelector("button");
+// btn.addEventListener('click', function(){
+//     console.log(this);
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue"
+// })
+
+
+
+//Long Method
+
+// let btn = document.querySelector("button");
+// let p = document.querySelector("p");
+// let h2 = document.querySelector("h2");
+// let h4 = document.querySelector("h4");
+
+
+// btn.addEventListener('click', function(){
+//     // console.log(this);
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue"
+// })
+
+// p.addEventListener('click', function(){
+//     // console.log(this);
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue"
+// })
+// h2.addEventListener('click', function(){
+//     // console.log(this);
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue"
+// })
+// h4.addEventListener('click', function(){
+//     // console.log(this);
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue"
+// })
+
+
+//Short Method
+
+let btn = document.querySelector("button");
+let p = document.querySelector("p");
+let h2 = document.querySelector("h2");
+let h4 = document.querySelector("h4");
+
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue"
+}
+
+btn.addEventListener('click', changeColor);
+
+p.addEventListener('click', changeColor);
+
+h2.addEventListener('click', changeColor);
+
+h4.addEventListener('click', changeColor);
